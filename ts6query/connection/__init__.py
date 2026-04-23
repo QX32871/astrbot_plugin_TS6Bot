1f503291b6bda131afc5e4b9315b6e711137f60f
QX32871 <qx32871111@qq.com>
2026-04-23 11:59:03 +0800

    fix(util): 根据官方文档的开发原则，将网络请求库换成aiohttp并重构网络请求部分的代码

--- ts6query/connection/__init__.py
+++ ts6query/connection/__init__.py
@@ -1,7 +1,13 @@
 """ts6query 连接模块"""
 
-from ts6query.connection.base import ConnectionBase
-from ts6query.connection.tcp import TCPConnection
+from ts6query.connection.base import ConnectionBase, ConnectionTimeout
+from ts6query.connection.tcp import TCPConnection, TCPConnectionPool
 from ts6query.connection.ssh import SSHConnection
 
-__all__ = ["ConnectionBase", "TCPConnection", "SSHConnection"]
+__all__ = [
+    "ConnectionBase",
+    "ConnectionTimeout",
+    "TCPConnection",
+    "TCPConnectionPool",
+    "SSHConnection",
+]
