1ccf0534ecd2b37566a538a066e991289205b67d
QX32871 <qx32871111@qq.com>
2026-04-23 12:50:32 +0800

    Revert "init repo"
    This reverts commit c66fa683745eafc27dfdae74e86071e5487587be.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("astrbot_plugin_ts6_info_fetcher", "QX32871", "TS6服务器信息查询插件", "1.0.0")
+@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
