4430c526cd0fb49d9757d6e18d05da91e71155f4
QX32871 <qx32871111@qq.com>
2026-04-23 12:50:32 +0800

    Revert "feat(utils): 初步完成TS6 ServerQuery工具类部分，插件部分尚待完善"
    This reverts commit acd0ac3b758a4b18538da25b40daee2903acb359.

--- ts6query/connection/__init__.py
+++
@@ -1,7 +0,0 @@
-"""ts6query 连接模块"""
-
-from ts6query.connection.base import ConnectionBase
-from ts6query.connection.tcp import TCPConnection
-from ts6query.connection.ssh import SSHConnection
-
-__all__ = ["ConnectionBase", "TCPConnection", "SSHConnection"]
--- ts6query/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""ts6query 数据模型"""
-
-from ts6query.models.server import ServerInfo, VirtualServerInfo
-from ts6query.models.user import UserInfo, ClientListEntry
-
-__all__ = ["ServerInfo", "VirtualServerInfo", "UserInfo", "ClientListEntry"]
